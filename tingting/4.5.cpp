62118b7308530bf35a029b41e08c1a8d3c692ee4
Gong_yihang <474356284@qq.com>
2020-04-22 22:01:37 +0800

    4.22

--- tingting/4.5.cpp
+++ tingting/4.5.cpp
@@ -22,28 +22,60 @@
 //     return 0;
 // }
 
+// #include <algorithm>
+// #include <iostream>
+// #include <math.h>
+// #include <string>
+// #include <vector>
+// using namespace std;
+
+// int main()
+// {
+//     int N;
+//     scanf("%d", &N);
+//     int sum = 1;
+//     int count = 1;
+//     int i;
+//     for (i = 3; sum <= N; i += 2) {
+//         sum += 2 * i;
+//         count += 2;
+//     }
+//     sum -= 2 * (i - 2);
+//     count -= 2;
+//     printf("%d %d\n", count, sum);
+//     system("pause");
+//     return 0;
+// }
 #include <algorithm>
 #include <iostream>
-#include <math.h>
+#include <cmath>
 #include <string>
 #include <vector>
 using namespace std;
-
+//http://39.105.108.26/problem.php?cid=1013&pid=1
 int main()
 {
-    int N;
-    scanf("%d", &N);
-    int sum = 1;
-    int count = 1;
-    int i;
-    for (i = 3; sum <= N; i += 2) {
-        sum += 2 * i;
-        count += 2;
+    int n;
+    cin >> n;
+    for (int i = n; i > 0; i--) {
+        for (int j = 1; j < n - i + 1; j++) {
+            printf(" ");
+        }
+        for (int k = i; k > 0; k--) {
+            printf("* ");
+        }
+        cout << endl;
     }
-    sum -= 2 * (i - 2);
-    count -= 2;
-    printf("%d %d\n", count, sum);
-    system("pause");
+
+    for (int i = 2; i <= n; i++) {
+        for (int j = n - i; j > 0; j--) {
+            printf(" ");
+        }
+        for (int k = 1; k <= i; k++) {
+            printf("* ");
+        }
+        cout << endl;
+    }
     return 0;
 }
 
