7f74c48a07da092a345137e3b197d597b73774cf
Gong_yihang <474356284@qq.com>
2020-05-17 21:57:17 +0800

    5.17

--- 5.13.cpp
+++ 5.13.cpp
@@ -67,33 +67,89 @@
 //     return 0;
 // }
 /*有一个N个元素的有序数列，输入一个数，把它插入到原有数列中相应位置，使得到的新数列仍保持有序，输出新数列*/
-#include <stdio.h>
-int main()
-{
-    int a[100];
-    // int a[10] = { 1, 2, 4, 5, 6, 7, 8 };
-    int n;
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
-    }
-    int b;
-    scanf("%d", &b);
-    int i = 0;
-    while (a[i] < b)
-        i++;
-    for (int j = n - 1; j >= i; j--) {
-        a[j + 1] = a[j];
-    }
-    a[i] = b;
-    for (int j = 0; j < n + 1; j++) {
-        printf("%d", a[j]);
-    }
-    return 0;
-}
+// #include <stdio.h>
+// int main()
+// {
+//     int a[100];
+//     // int a[10] = { 1, 2, 4, 5, 6, 7, 8 };
+//     int n;
+//     scanf("%d", &n);
+//     for (int i = 0; i < n; i++) {
+//         scanf("%d", &a[i]);
+//     }
+//     int b;
+//     scanf("%d", &b);
+//     int i = 0;
+//     while (a[i] < b)
+//         i++;
+//     for (int j = n - 1; j >= i; j--) {
+//         a[j + 1] = a[j];
+//     }
+//     a[i] = b;
+//     for (int j = 0; j < n + 1; j++) {
+//         printf("%d", a[j]);
+//     }
+//     return 0;
+// }
 
 // 1 2 3 4 5
 // 6 7 8 9 10
 // 11 12 13 14 15
 // 16 17 18 19 20
 // 21 22 23 24 25
+
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int a[100][100];
+    int n, m;
+    scanf("%d %d",&n,&m);
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j < m;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    int averagen[100],averagem[100];
+    int sumn = 0, summ = 0;
+    for(int i = 0;i < n;i++){
+        for(int j = 0;j < m;j++){
+            sumn += a[i][j];
+        }
+        averagen[i] = sumn / m;
+        sumn = 0;
+    }
+
+    for(int i = 0;i < m;i++){
+        for(int j = 0;j < n;j++){
+            summ += a[j][i];
+        }
+        averagem[i] = summ / n;
+        summ = 0;
+    }
+
+    for(int i = 0;i < n;i++){
+        printf("%d ", averagen[i]);
+    }
+    printf("\n");
+    for(int i = 0;i < m;i++){
+        printf("%d ", averagem[i]);
+    }
+
+
+    system("pause");
+    return 0;
+}
+
+// 3 5
+// 100 90 80 70 60
+// 90 80 70 80 80
+// 80 60 70 60 60
+// 100 100 100 100 100
+// 90 90 80 80 70
