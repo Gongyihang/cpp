eccffdbb5e920a9ab813d38679532efd806810f6
Gong_yihang <474356284@qq.com>
2020-05-23 23:42:51 +0800

    5.23

--- 5.13.cpp
+++ 5.13.cpp
@@ -5,7 +5,6 @@
 // #include <cmath>
 // using namespace std;
 
-
 // int main(){
 //     float a,b,c = 0;
 //     scanf("%f %f",&a,&b);
@@ -136,7 +135,6 @@ int main(){
         averagem[i] = summ / n;
         summ = 0;
     }
-    
 
     for(int i = 0;i < n;i++){
         printf("%d ", averagen[i]);
