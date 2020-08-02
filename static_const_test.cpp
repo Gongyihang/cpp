9c3cac1624e5a86c597e27816a0cdab0f5317817
gyh <474356284@qq.com>
2020-08-02 22:21:43 +0800

    8.2

---
+++ static_const_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <math.h>
+using namespace std;
+
+int main(){
+    static int v = 1;
+    v++;
+    cout << v << endl;
+    system("pause");
+    return 0;
+}
