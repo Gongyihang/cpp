2c3408d6148264d3ad7b9176d53ed92d572651ce
gyh <474356284@qq.com>
2020-08-19 19:36:59 +0800

    8.19

---
+++ 05inverse_tree.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <math.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x): val(x), left(NULL), right(NULL){}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL)
+        return NULL;
+        TreeNode* t = root->left;
+        root->left = root->right;
+        root->right = t;
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
+
+
+int main(){
+    
+    system("pause");
+    return 0;
+}
