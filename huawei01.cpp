0ca21769521793397c1c1f392560327c66eabbd0
gyh <474356284@qq.com>
2020-09-02 18:49:46 +0800

    9.2

---
+++ huawei01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <math.h>
+using namespace std;
+
+int main(){
+    
+    system("pause");
+    return 0;
+}
