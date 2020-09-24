fcc66f2a1c3744653f3a36ecf4a1626dae92e240
gyh <474356284@qq.com>
2020-09-24 23:27:52 +0800

    9.24

--- xiaomi01.cpp
+++ xiaomi01.cpp
@@ -1,32 +1,36 @@
-#include <iostream>
 #include <algorithm>
-#include <vector>
-#include <string>
-#include <map>
-#include <stack>
-#include <queue>
 #include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
 using namespace std;
 
-string remove(string s){
+string remove(string s)
+{
     string t;
     int i, j;
-    t.append(s,0,1);
-    for(i = 1;i < (int)s.size();i++){
-        for( j = 0;j < (int)t.size();j++){
-            if(s[i] == t[j]) break;
+    t.append(s, 0, 1);
+    for (i = 1; i < (int)s.size(); i++) {
+        for (j = 0; j < (int)t.size(); j++) {
+            if (s[i] == t[j])
+                break;
         }
-        if(j == (int)t.size()) t.append(s,i,1);
+        if (j == (int)t.size())
+            t.append(s, i, 1);
     }
     return t;
 }
 
-int main(){
+int main()
+{
     string s;
-    getline(cin,s);
+    getline(cin, s);
     string res = remove(s);
 
     cout << res << endl;
-    
+
     return 0;
 }
