ef96ff399fb6015de103692c2fe0d074624cc641
gyh <474356284@qq.com>
2020-10-15 21:25:32 +0800

    10.15

---
+++ shenxinfu01.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
+#include <stack>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+struct point{
+    int x;
+    int y;
+};
+
+int main(){
+    vector<vector<int>> m(10,vector<int>(10,0));
+    point p;
+    cin >> p.x >> p.y;
+    int count = 0;
+    for(int i = 0;i < 10;i++){
+        string s;
+        cin >> s;
+        for(int j = 0;j < 10;j++){
+            m[i][j] = s[j] - '0';
+            if(m[i][j] != 0) count++;
+        }
+    }
+
+    if(count == 0)
+    cout << 1 << endl;
+    else cout << 2 << endl;
+    return 0;
+}
