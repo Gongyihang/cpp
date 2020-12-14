bdda12d67339f49dbe4b13907d0eb1eadb0e3fdc
gyh <474356284@qq.com>
2020-12-14 16:16:04 +0800

    12.14

---
+++ 20201214.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
+#include <stack>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+int main(){
+    cout << 1111 << endl;
+    system("pause");
+    return 0;
+}
